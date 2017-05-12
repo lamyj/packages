f24ad4ab2d69d9bed8829dd040ba1280e750ff47
Julien Lamy <lamy@unistra.fr>
2017-05-12 14:29:26 +0200

    Update odil/jessie with backported DCMTK

---
+++ apt/entry.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -u
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y --no-install-recommends adduser sudo
+
+adduser --disabled-password --gecos '' somebody
+chown -R somebody:somebody ~somebody
+echo 'somebody ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+sudo -u somebody -H -E "$@"
